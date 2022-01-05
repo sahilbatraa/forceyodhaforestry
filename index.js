37e237bc92c590baa20ea2fa158b87b85852a814
sahilbatraa <sahilbatraa@gmail.com>
2022-01-05 23:36:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = themeConfig => {
    * Default theme configuration
    */
   themeConfig = Object.assign(themeConfig, {
-    logo: '/assets/img/logo.png',
+    logo: '/assets/img/mylogo.png',
     nav: themeConfig.nav || [
       {
         text: 'Blog',
