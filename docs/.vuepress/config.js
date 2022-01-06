0db125fa14ffbe8dd0f68d21a952ea6c08c87802
Sahil <sahilbatraa@gmail.com>
2022-01-06 14:26:58 +0530

    a

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,7 +22,7 @@ module.exports = {
         },
         {
           type: 'facebook',
-          link: '#',
+          link: 'https://mtxb2b.com',
         },
         {
           type: 'github',
