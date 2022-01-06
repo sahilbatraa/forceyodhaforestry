313dcfdf27c72e6e107c623388b8677298afb437
Sahil <sahilbatraa@gmail.com>
2022-01-06 14:13:35 +0530

    a

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -82,16 +82,6 @@ module.exports = {
     sitemap: {
       hostname: 'https://github.com/wowthemesnet/vuepress-theme-mediumish/'
     },
-    comment: {
-      service: 'disqus',
-      shortname: 'demowebsite',
-    },
-    newsletter: {
-      endpoint: 'https://wowthemes.us11.list-manage.com/subscribe/post?u=8aeb20a530e124561927d3bd8&id=8c3d2d214b'
-    },
-    feed: {
-      canonical_base: 'https://github.com/wowthemesnet/vuepress-theme-mediumish/',
-    },
     smoothScroll: true
   },
 }
